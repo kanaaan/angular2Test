7ad72cbfe23bc2775c19bc1c13be4b647399ec10
Kanaaan <mapjs@mapjs.com>
2016-10-05 00:12:10 +0200

    * Enhance tableitem.component.ts

--- app/tableitem.component.ts
+++ app/tableitem.component.ts
@@ -4,14 +4,17 @@ import { Component } from '@angular/core';
     selector: 'tableitem-ctrl',
     template: `
     <div>
-        <th>Firstname</th>
-        <th>Lastname</th> 
+        <th>{{Firstname}}</th>
+        <th>{{Lastname}}</th> 
     </div>
     `
 })
 
 
 export class TableItemComponent {
+    Firstname: string ='Firstname';
+    Lastname: string ='Lastname';
+
     constructor() {
         console.log('TableItemComponent constructor');
     }    
