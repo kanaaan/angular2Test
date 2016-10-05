4892115f94b024a57b6633110cb02407b9523e1a
Kanaaan <mapjs@mapjs.com>
2016-10-05 23:26:09 +0200

    * Add class Person

--- app/table.component.ts
+++ app/table.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { TableItemComponent } from './tableitem.component';
 
+class Person {
+  Firstname: string ;
+  Lastname: string ;
+}
+
 @Component({
     selector: 'table-ctrl',
     template: '\
     <h1>TableComponent</h1>\
-    <input type="text" class="form-control" placeholder="Firstname">\
+    <input #iFirstname type="text" class="form-control" placeholder="Firstname" (blur)="this.person.Firstname=iFirstname.value"/>\
     <h1></h1>\
-    <input type="text" class="form-control" placeholder="Lastname">\
+    <input #iLastname type="text" class="form-control" placeholder="Lastname" (blur)="AddLastname(iLastname.value)"/>\
     <h1></h1>\
     <button class="btn btn-success" (click)="AddClicked($event)">Add</button>\
     <button  class="btn btn-warning" (click)="RemoveClicked($event)">Remove</button>\
@@ -22,20 +27,29 @@ import { TableItemComponent } from './tableitem.component';
 })
 
 
-export class TableComponent {
-    data = [ 1,2,3,4 ];
 
+export class TableComponent {
+    
+    data: Person[] = [{Firstname: 'a', Lastname: 'b'}];
+    
+    person: Person;
+    
     constructor() {
         console.log('TableComponent constructor');
+        this.person=new Person();
     }
     public AddClicked(event)
     {
-        console.log('AddClicked');
-        this.data.push(this.data.length+1);
+        console.log('AddClicked '+this.person.Firstname+' '+this.person.Lastname);
+        this.data.push(this.person);
     }
     public RemoveClicked(event)
     {
         console.log('RemoveClicked');
         this.data.splice(this.data.length-1);
-    }  
+    } 
+
+    AddLastname(a: string) {
+        this.person.Lastname=a;
+    }
  }
